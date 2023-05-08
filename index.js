958c8d971fdb8c2840d156944d6c0784676a8591
Benhur Stephen <92089774+Benlite777@users.noreply.github.com>
2023-05-08 16:19:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (randomNumber1 > randomNumber2)
 // Player2 Wins
 else if (randomNumber2 > randomNumber1)
 {
-    document.querySelector("h1").innerHTML = " 🚩 Player 2 Wins";
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
 }
 // Draw Game
 else
